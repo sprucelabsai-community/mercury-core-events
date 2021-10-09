ac727d11c7523d2550e912ec7bbadf18debd8834
CircleCI <devops@sprucelabs.ai>
2021-10-09 23:45:43 +0000

    chore(auto-upgrade): upgraded packages [upgrade-all]

--- src/.spruce/events/mercury/setRole.v2020_12_25.contract.ts
+++ src/.spruce/events/mercury/setRole.v2020_12_25.contract.ts
@@ -1,39 +1,41 @@
-import { buildEventContract } from '@sprucelabs/mercury-types'
-import { buildPermissionContract } from '@sprucelabs/mercury-types'
 import setRoleEmitTargetAndPayloadSchema from '#spruce/schemas/mercury/v2020_12_25/setRoleEmitTargetAndPayload.schema'
 import setRoleResponsePayloadSchema from '#spruce/schemas/mercury/v2020_12_25/setRoleResponsePayload.schema'
+import { buildEventContract } from '@sprucelabs/mercury-types'
+import { buildPermissionContract } from '@sprucelabs/mercury-types'
+
 
 const setRoleEventContract = buildEventContract({
-	eventSignatures: {
-		'set-role::v2020_12_25': {
-			emitPayloadSchema: setRoleEmitTargetAndPayloadSchema,
-			responsePayloadSchema: setRoleResponsePayloadSchema,
-			emitPermissionContract: buildPermissionContract({
-				id: 'hiring-and-promoting',
-				name: 'Hiring and promoting contract',
-				description:
-					'Various permissions related to emitting hiring and promoting people at organizations and locations.',
-				requireAllPermissions: false,
-				permissions: [
-					{
-						id: 'hiring-and-promoting',
-						name: 'Hire a person',
-						defaults: {
-							owner: {
-								default: true,
-							},
-							groupManager: {
-								clockedIn: true,
-							},
-							manager: {
-								clockedIn: true,
-							},
-						},
-					},
-				],
-			}),
-		},
-	},
+    eventSignatures: {
+        'set-role::v2020_12_25': {
+            
+            emitPayloadSchema: setRoleEmitTargetAndPayloadSchema,
+            responsePayloadSchema: setRoleResponsePayloadSchema,
+            emitPermissionContract: buildPermissionContract({
+  "id": "hiring-and-promoting",
+  "name": "Hiring and promoting contract",
+  "description": "Various permissions related to emitting hiring and promoting people at organizations and locations.",
+  "requireAllPermissions": false,
+  "permissions": [
+    {
+      "id": "hiring-and-promoting",
+      "name": "Hire a person",
+      "defaults": {
+        "owner": {
+          "default": true
+        },
+        "groupManager": {
+          "clockedIn": true
+        },
+        "manager": {
+          "clockedIn": true
+        }
+      }
+    }
+  ]
+}),
+            
+        }
+    }
 })
 export default setRoleEventContract
 
