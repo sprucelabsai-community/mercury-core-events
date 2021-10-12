0bbbd04ef4411c2a79a8213ea2aaf124fb05f58a
CircleCI <devops@sprucelabs.ai>
2021-10-12 13:45:56 +0000

    chore(auto-upgrade): upgraded packages [upgrade-all]

--- src/.spruce/events/mercury/createLocation.v2020_12_25.contract.ts
+++ src/.spruce/events/mercury/createLocation.v2020_12_25.contract.ts
@@ -1,14 +1,18 @@
-import { buildEventContract } from '@sprucelabs/mercury-types'
 import createLocationEmitTargetAndPayloadSchema from '#spruce/schemas/mercury/v2020_12_25/createLocationEmitTargetAndPayload.schema'
 import createLocationResponsePayloadSchema from '#spruce/schemas/mercury/v2020_12_25/createLocationResponsePayload.schema'
+import { buildEventContract } from '@sprucelabs/mercury-types'
+
 
 const createLocationEventContract = buildEventContract({
-	eventSignatures: {
-		'create-location::v2020_12_25': {
-			emitPayloadSchema: createLocationEmitTargetAndPayloadSchema,
-			responsePayloadSchema: createLocationResponsePayloadSchema,
-		},
-	},
+    eventSignatures: {
+        'create-location::v2020_12_25': {
+            
+            emitPayloadSchema: createLocationEmitTargetAndPayloadSchema,
+            responsePayloadSchema: createLocationResponsePayloadSchema,
+            
+            
+        }
+    }
 })
 export default createLocationEventContract
 
--- src/.spruce/events/mercury/updatePerson.v2020_12_25.contract.ts
+++ src/.spruce/events/mercury/updatePerson.v2020_12_25.contract.ts
@@ -1,14 +1,18 @@
-import { buildEventContract } from '@sprucelabs/mercury-types'
 import updatePersonEmitTargetAndPayloadSchema from '#spruce/schemas/mercury/v2020_12_25/updatePersonEmitTargetAndPayload.schema'
 import updatePersonResponsePayloadSchema from '#spruce/schemas/mercury/v2020_12_25/updatePersonResponsePayload.schema'
+import { buildEventContract } from '@sprucelabs/mercury-types'
+
 
 const updatePersonEventContract = buildEventContract({
-	eventSignatures: {
-		'update-person::v2020_12_25': {
-			emitPayloadSchema: updatePersonEmitTargetAndPayloadSchema,
-			responsePayloadSchema: updatePersonResponsePayloadSchema,
-		},
-	},
+    eventSignatures: {
+        'update-person::v2020_12_25': {
+            
+            emitPayloadSchema: updatePersonEmitTargetAndPayloadSchema,
+            responsePayloadSchema: updatePersonResponsePayloadSchema,
+            
+            
+        }
+    }
 })
 export default updatePersonEventContract
 
