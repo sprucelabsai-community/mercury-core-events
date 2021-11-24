3d53965f41ac02bcd07faf9ba27f02376caf321d
CircleCI <devops@sprucelabs.ai>
2021-11-24 04:47:38 +0000

    chore(auto-upgrade): upgraded packages [upgrade-all]

---
+++ src/.spruce/schemas/mercury/v2020_12_25/listInstalledSkillsResponsePayload.schema.ts
@@ -0,0 +1,24 @@
+import { SchemaRegistry } from '@sprucelabs/schema'
+import { SpruceSchemas } from '../../schemas.types'
+
+import listInstalledSkillsSkillsResponsePayloadSchema_v2020_12_25 from '#spruce/schemas/mercury/v2020_12_25/listInstalledSkillsSkillsResponsePayload.schema'
+
+const listInstalledSkillsResponsePayloadSchema: SpruceSchemas.Mercury.v2020_12_25.ListInstalledSkillsResponsePayloadSchema  = {
+	id: 'listInstalledSkillsResponsePayload',
+	version: 'v2020_12_25',
+	namespace: 'Mercury',
+	name: '',
+	    fields: {
+	            /** . */
+	            'skills': {
+	                type: 'schema',
+	                isRequired: true,
+	                isArray: true,
+	                options: {schema: listInstalledSkillsSkillsResponsePayloadSchema_v2020_12_25,}
+	            },
+	    }
+}
+
+SchemaRegistry.getInstance().trackSchema(listInstalledSkillsResponsePayloadSchema)
+
+export default listInstalledSkillsResponsePayloadSchema
---
+++ src/.spruce/schemas/mercury/v2020_12_25/listInstalledSkillsSkillsResponsePayload.schema.ts
@@ -0,0 +1,55 @@
+import { SchemaRegistry } from '@sprucelabs/schema'
+import { SpruceSchemas } from '../../schemas.types'
+
+
+
+const listInstalledSkillsSkillsResponsePayloadSchema: SpruceSchemas.Mercury.v2020_12_25.ListInstalledSkillsSkillsResponsePayloadSchema  = {
+	id: 'listInstalledSkillsSkillsResponsePayload',
+	version: 'v2020_12_25',
+	namespace: 'Mercury',
+	name: '',
+	    fields: {
+	            /** Id. */
+	            'id': {
+	                label: 'Id',
+	                type: 'id',
+	                isRequired: true,
+	                options: undefined
+	            },
+	            /** Name. */
+	            'name': {
+	                label: 'Name',
+	                type: 'text',
+	                isRequired: true,
+	                options: undefined
+	            },
+	            /** Description. */
+	            'description': {
+	                label: 'Description',
+	                type: 'text',
+	                options: undefined
+	            },
+	            /** Slug. */
+	            'slug': {
+	                label: 'Slug',
+	                type: 'text',
+	                isRequired: true,
+	                options: undefined
+	            },
+	            /** . */
+	            'dateCreated': {
+	                type: 'number',
+	                isRequired: true,
+	                options: undefined
+	            },
+	            /** . */
+	            'dateDeleted': {
+	                type: 'number',
+	                options: undefined
+	            },
+	    }
+}
+
+SchemaRegistry.getInstance().trackSchema(listInstalledSkillsSkillsResponsePayloadSchema)
+
+export default listInstalledSkillsSkillsResponsePayloadSchema
