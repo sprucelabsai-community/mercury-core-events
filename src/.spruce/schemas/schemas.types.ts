b1be76954eda6592ea9123131770436e0931d295
CircleCI <devops@sprucelabs.ai>
2022-11-26 17:42:30 +0000

    chore(auto-upgrade): upgraded packages [upgrade-all]

--- src/.spruce/schemas/schemas.types.ts
+++ src/.spruce/schemas/schemas.types.ts
@@ -6994,6 +6994,7 @@ declare module '@sprucelabs/spruce-core-schemas/build/.spruce/schemas/core.schem
 			                type: 'schema',
 			                isRequired: true,
 			                isArray: true,
+			                minArrayLength: 0,
 			                options: {schema: SpruceSchemas.Mercury.v2020_12_25.FullPermissionContractSchema,}
 			            },
 			    }
