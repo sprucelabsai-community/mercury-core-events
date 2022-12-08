606d07b382110789461f4d95410b56a6a1c0bdf1
Taylor Romero <me@taylorrome.ro>
2022-12-07 23:56:04 -0700

    patch: perm fixes

--- src/.spruce/permissions/permissions.types.ts
+++ src/.spruce/permissions/permissions.types.ts
@@ -1,17 +1,5 @@
 declare module '@sprucelabs/mercury-types/build/types/mercury.types' {
 	interface PermissionContractMap {
-		'locations-contract': [
-			'can-create-location','can-update-location','can-delete-location','can-view-private-locations-at-own-org',
-		]
-		'organizations-contract': [
-			'can-update-organization-profile','can-delete-organization','can-view-private-organizations',
-		]
-		'roles-contract': [
-			'can-create-role','can-update-role','can-delete-role','can-view-private-roles-at-own-org','can-view-persons-roles',
-		]
-		'people-contract': [
-			'can-create-person','can-list-people-permission-id','can-scramble-other-person','can-get-anonymous-person','can-get-person-private-fields','can-get-person-by-phone','can-listen-to-did-signup','can-listen-to-did-update',
-		]
 		'events-contract': [
 			'can-register-global-events',
 		]
@@ -24,17 +12,26 @@ declare module '@sprucelabs/mercury-types/build/types/mercury.types' {
 		'listeners-contract': [
 			'can-register-global-listeners',
 		]
-		'skills-contract': [
-			'can-install-skills','can-uninstall-skills','can-view-marketplace','can-check-if-skill-is-installed',
+		'locations-contract': [
+			'can-create-location','can-update-location','can-delete-location','can-view-private-locations-at-own-org',
 		]
 		'messaging-contract': [
 			'can-emit-did-message-event','can-listen-to-did-message-event','can-message-others',
 		]
-		'permission-contracts': [
+		'organizations-contract': [
+			'can-update-organization-profile','can-delete-organization','can-view-private-organizations',
+		]
+		'people-contract': [
+			'can-create-person','can-list-people-permission-id','can-scramble-other-person','can-get-anonymous-person','can-get-person-private-fields','can-get-person-by-phone','can-listen-to-did-signup','can-listen-to-did-update',
+		]
+		'permission-contract': [
 			'can-get-permission-contracts-for-org','can-list-permission-contracts-for-org','can-upsert-permission-contract-for-org',
 		]
-		'permissions-contract': [
-			'can-upsert-permission-contract-for-org',
+		'roles-contract': [
+			'can-create-role','can-update-role','can-delete-role','can-view-private-roles-at-own-org','can-view-persons-roles',
+		]
+		'skills-contract': [
+			'can-install-skills','can-uninstall-skills','can-view-marketplace','can-check-if-skill-is-installed',
 		]
 	}
 }
