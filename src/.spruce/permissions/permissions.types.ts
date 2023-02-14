07dab3ed25cbae2c026aa1437080fff6e2401cf3
CircleCI <devops@sprucelabs.ai>
2023-02-14 07:18:33 +0000

    chore(auto-upgrade): upgraded packages [upgrade-all]

--- src/.spruce/permissions/permissions.types.ts
+++ src/.spruce/permissions/permissions.types.ts
@@ -1,5 +1,8 @@
 declare module '@sprucelabs/mercury-types/build/types/mercury.types' {
 	interface PermissionContractMap {
+		'events-contract': [
+			'can-register-global-events',
+		]
 		'feed-contract': [
 			'can-see-other-persons-feed','can-subscribe-to-feed-updates',
 		]
@@ -21,17 +24,14 @@ declare module '@sprucelabs/mercury-types/build/types/mercury.types' {
 		'people-contract': [
 			'can-create-person','can-list-people-permission-id','can-scramble-other-person','can-get-anonymous-person','can-get-person-private-fields','can-get-person-by-phone','can-listen-to-did-signup','can-listen-to-did-update',
 		]
+		'permission-contract': [
+			'can-get-permission-contracts-for-org','can-list-permission-contracts-for-org','can-upsert-permission-contract-for-org','can-list-permission-contracts-globally','can-save-permissions-globally','can-load-others-permissions',
+		]
 		'roles-contract': [
 			'can-create-role','can-update-role','can-delete-role','can-view-private-roles-at-own-org','can-view-persons-roles',
 		]
 		'skills-contract': [
 			'can-install-skills','can-uninstall-skills','can-view-marketplace','can-check-if-skill-is-installed',
-		]
-		'events-contract': [
-			'can-register-global-events',
-		]
-		'permission-contract': [
-			'can-get-permission-contracts-for-org','can-list-permission-contracts-for-org','can-upsert-permission-contract-for-org','can-list-permission-contracts-globally','can-save-permissions-globally','can-load-others-permissions',
 		]
 	}
 }
