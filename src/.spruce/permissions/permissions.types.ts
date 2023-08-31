b8cfa07e5d83e0701d07a000121f3756b4d51ee8
CircleCI <devops@sprucelabs.ai>
2023-08-31 09:16:28 +0000

    chore(auto-upgrade): upgraded packages [upgrade-all]

--- src/.spruce/permissions/permissions.types.ts
+++ src/.spruce/permissions/permissions.types.ts
@@ -2,42 +2,6 @@ import '@sprucelabs/mercury-types'
 
 declare module '@sprucelabs/mercury-types/build/types/mercury.types' {
 	interface PermissionContractMap {
-		'chatbot-contract': [
-			'can-register-chatbots',
-		]
-		'events-contract': [
-			'can-register-global-events',
-		]
-		'feed-contract': [
-			'can-see-other-persons-feed','can-subscribe-to-feed-updates',
-		]
-		'hiring-and-promoting-contract': [
-			'can-hire-person','can-fire-person','hire-and-promote-when-not-installed',
-		]
-		'listeners-contract': [
-			'can-register-global-listeners',
-		]
-		'locations-contract': [
-			'can-create-location','can-update-location','can-delete-location','can-view-private-locations-at-own-org',
-		]
-		'messaging-contract': [
-			'can-emit-did-message-event','can-listen-to-did-message-event','can-message-others','can-message-as-sprucebot',
-		]
-		'organizations-contract': [
-			'can-update-organization-profile','can-delete-organization','can-view-private-organizations','can-list-orgs-for-other-people',
-		]
-		'permission-contract': [
-			'can-get-permission-contracts-for-org','can-list-permission-contracts-for-org','can-upsert-permission-contract-for-org','can-list-permission-contracts-globally','can-save-permissions-globally','can-load-others-permissions',
-		]
-		'people-contract': [
-			'can-create-person','can-list-people-permission-id','can-scramble-other-person','can-edit-other-people','can-get-anonymous-person','can-get-person-private-fields','can-get-person-by-phone','can-listen-to-did-signup','can-listen-to-did-update','can-get-other-person',
-		]
-		'roles-contract': [
-			'can-create-role','can-update-role','can-delete-role','can-view-private-roles-at-own-org','can-view-persons-roles',
-		]
-		'skills-contract': [
-			'can-install-skills','can-uninstall-skills','can-view-marketplace','can-check-if-skill-is-installed',
-		]
 	}
 }
 
