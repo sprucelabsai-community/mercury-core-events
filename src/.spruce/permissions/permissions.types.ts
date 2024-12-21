822f1349023f9800c39b3fc473904ab286525872
Taylor Romero <me@taylorrome.ro>
2024-12-21 09:43:45 -0700

    major:upgrade

--- src/.spruce/permissions/permissions.types.ts
+++ src/.spruce/permissions/permissions.types.ts
@@ -2,12 +2,6 @@ import '@sprucelabs/mercury-types'
 
 declare module '@sprucelabs/mercury-types/build/types/mercury.types' {
 	interface PermissionContractMap {
-		'authorizer-contract': [
-			'can-update-others-authorizer-status',
-		]
-		'chatbot-contract': [
-			'can-register-chatbots',
-		]
 		'events-contract': [
 			'can-register-global-events',
 		]
@@ -40,6 +34,15 @@ declare module '@sprucelabs/mercury-types/build/types/mercury.types' {
 		]
 		'skills-contract': [
 			'can-install-skills','can-uninstall-skills','can-view-marketplace','can-check-if-skill-is-installed',
+		]
+		'chat-bot-contract': [
+			'can-register-chat-bots',
+		]
+		'chatbot-contract': [
+			'can-register-chatbots',
+		]
+		'authorizer-contract': [
+			'can-update-others-authorizer-status',
 		]
 	}
 }
